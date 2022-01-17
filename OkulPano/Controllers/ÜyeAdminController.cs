425f310fd49ba2908fec08d1ceab589ec3d8e321
YTzcn <YTZCN>
2022-01-17 16:13:32 +0300

    17.01.2022

--- OkulPano/Controllers/ÜyeAdminController.cs
+++ OkulPano/Controllers/ÜyeAdminController.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace OkulPano.Controllers
 {
     public class ÜyeAdminController : Controller
     {
         // GET: ÜyeAdmin
+        //View Olarak Layout Kullanma.
+
         public ActionResult Index()
         {
             return View();
