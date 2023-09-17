a20d54cfb17b1090e8d1a79c848bc744a7d0c469
Yahya Tezcan <yahya.tezcan@hotmail.com>
2023-09-17 18:04:15 +0300

    ,

--- OkulPano/Controllers/PanelController.cs
+++ OkulPano/Controllers/PanelController.cs
@@ -1,4 +1,4 @@
-﻿    using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections;
 using System.Globalization;
@@ -23,28 +23,45 @@ namespace OkulPano.Controllers
             ViewBag.OkulAd = Kullanıcı.Ad;
             System.IO.FileInfo ff = new System.IO.FileInfo(KullanıcıR.ResimYol);
             string DosyaUzantisi = ff.Extension;
-            ViewBag.Logo =  Kullanıcı.OkulId+DosyaUzantisi;
+            ViewBag.Logo = Kullanıcı.OkulId + DosyaUzantisi;
             var NöbetYerler = context.NöbertYers.Where(x => x.OkulId == Kullanıcı.OkulId && x.Aktiflik == true).ToList();
             var öğretmen = context.Öğretmens.Where(x => x.OkulId == Kullanıcı.OkulId).ToList();
             ViewBag.öğretmendeger = öğretmen;
-            ViewBag.Kayaryazı = context.Okuls.Where(x => x.OkulId == Kullanıcı.OkulId).Select(x=>x.KayarYazı).FirstOrDefault();
+            ViewBag.Kayaryazı = context.Okuls.Where(x => x.OkulId == Kullanıcı.OkulId).Select(x => x.KayarYazı).FirstOrDefault();
             ViewBag.Tarih = DateTime.Now.ToString();
             Queue queue = new Queue();
 
             ViewBag.que = queue;
+            string[,] PanoModel = new string[13, 2]
+            {
+                {"Bahçe","Ahmet Öğretmen" },
+                {"Giriş Kat","Mahmut Öğretmen" },
+                {"1.Kat","Şule Öğretmen" },
+                {"2.Kat","Ayhan Öğretmen" },
+                {"3.Kat","Hayriye Öğretmen" },
+                {"4.Kat","Canan Öğretmen" },
+                {"5.Kat","Meryem Öğretmen" },
+                {"6.Kat","Kadir Öğretmen" },
+                {"7.Kat","Aslı Öğretmen" },
+                {"8.Kat","Metin Öğretmen" },
+                {"9.Kat","Süleyman Öğretmen" },
+                {"10.Kat","Tanju Öğretmen" },
+                {"Kantin","Cevriye Öğretmen" }
+            };
+            ViewBag.Nöbet = PanoModel;
 
-            
-            var KayanResimList = context.KayanResims.Where(x => x.OkulId == Kullanıcı.OkulId).Select(x=>x.ResimYol).ToList();
+
+            var KayanResimList = context.KayanResims.Where(x => x.OkulId == Kullanıcı.OkulId).Select(x => x.ResimYol).ToList();
             ViewBag.KayanList = KayanResimList;
             string Gün = CultureInfo.CurrentCulture.DateTimeFormat.DayNames[(int)DateTime.Now.DayOfWeek];
             Dictionary<NöbertYer, List<Öğretmen>> model = new Dictionary<NöbertYer, List<Öğretmen>>();
-            
+
             List<NöbertYer> GünlükNöbetYerleri = new List<NöbertYer>();
             GünlükNöbetYerleri.AddRange(context.NöbertYers.Where(x => x.OkulId == Kullanıcı.OkulId));
             foreach (NöbertYer nöbetYer in GünlükNöbetYerleri)
             {
-                List<Öğretmen> OGünküNöbetçiler= new List<Öğretmen>();
-                OGünküNöbetçiler.AddRange(context.Öğretmens.Where(x => x.NöbetGün== Gün && x.NoöetYerId == nöbetYer.Id));
+                List<Öğretmen> OGünküNöbetçiler = new List<Öğretmen>();
+                OGünküNöbetçiler.AddRange(context.Öğretmens.Where(x => x.NöbetGün == Gün && x.NoöetYerId == nöbetYer.Id));
                 model.Add(nöbetYer, OGünküNöbetçiler);
             }
 
@@ -54,10 +71,10 @@ namespace OkulPano.Controllers
 
 
             return View();
-    
-        
+
+
         }
-    
-    
+
+
     }
 }
